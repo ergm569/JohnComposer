d369fd26777dea597165dcc3333b22f32a9e845b
ergm569 <er.gm569@gmail.com>
2018-07-25 23:39:41 -0500

    Jython
    Descubrí el Jython y el sentido de todo cambio

---
+++ Main.py
@@ -0,0 +1,19 @@
+# playNote.py
+# Demonstrates how to play a single note.
+ 
+from music import *        # import music library
+ 
+
+#note = Note(C4, HN)        # create a middle C half note
+#Play.midi(note)            # and play it!
+
+
+
+/********
+PROBAR correr esto en python.
+dentro de music.py está todo.
+
+
+No debería funcionar porque los archivos están
+escritos para ser interpretados por jython 
+no por python  ****/
